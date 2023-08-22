9e139197797c97d7b7989bfdf496fe5b0584ccc4
Raul <raul.rhf9316@gmail.com>
2023-08-22 15:28:52 -0300

    service mae

--- MaternidadeN1-Edna-Raul/MaternidadeN1-Edna-Raul/Interfaces/IMaeService.cs
+++ MaternidadeN1-Edna-Raul/MaternidadeN1-Edna-Raul/Interfaces/IMaeService.cs
@@ -1,11 +1,18 @@
-﻿using MaternidadeN1_Edna_Raul.Models;
+﻿using MaternidadeN1_Edna_Raul.DTOs;
+using MaternidadeN1_Edna_Raul.Models;
 
 namespace MaternidadeN1_Edna_Raul.Interfaces
 {
     public interface IMaeService
     {
         Task<MaeModel> GetMaeByID(int id);
+        Task<MaeModel> GetMaeByCPF(int id);
+        Task<List<RecemNascidoModel>> GetMaesEstadoCivil(string estadoCivil);
+        Task<List<RecemNascidoModel>> GetAllMaes();
         Task<List<RecemNascidoModel>> GetRNsPorMae(int id);
-        Task<List<RecemNascidoModel>> GetRNsPorMae(int id);
+        Task<MaeModel> PostMae(MaeDTO maeRequest);
+        Task<MaeModel> UpdateMae(int id, MaeDTO maeRequest);
+
+        //todo fazer PATCH
     }
 }
--- MaternidadeN1-Edna-Raul/MaternidadeN1-Edna-Raul/Services/MaeServices.cs
+++ MaternidadeN1-Edna-Raul/MaternidadeN1-Edna-Raul/Services/MaeServices.cs
@@ -1,6 +1,44 @@
-﻿namespace MaternidadeN1_Edna_Raul.Services
+﻿using MaternidadeN1_Edna_Raul.DTOs;
+using MaternidadeN1_Edna_Raul.Interfaces;
+using MaternidadeN1_Edna_Raul.Models;
+
+namespace MaternidadeN1_Edna_Raul.Services
 {
-    public class MaeServices
+    public class MaeServices : IMaeService
     {
+        public Task<List<RecemNascidoModel>> GetAllMaes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MaeModel> GetMaeByCPF(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MaeModel> GetMaeByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<RecemNascidoModel>> GetMaesEstadoCivil(string estadoCivil)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<RecemNascidoModel>> GetRNsPorMae(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MaeModel> PostMae(MaeDTO maeRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MaeModel> UpdateMae(int id, MaeDTO maeRequest)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
