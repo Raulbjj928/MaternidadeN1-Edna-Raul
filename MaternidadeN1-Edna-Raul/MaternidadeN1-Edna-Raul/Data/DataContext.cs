e7fb693c7a1d89d28732aba0add25d07dfffe8b6
Edna de Paula <ednadepaulaeap@gmail.com>
2023-08-22 15:07:43 -0300

    datacontext criada

---
+++ MaternidadeN1-Edna-Raul/MaternidadeN1-Edna-Raul/Data/DataContext.cs
@@ -0,0 +1,32 @@
+﻿using MaternidadeN1_Edna_Raul.Models;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace MaternidadeN1_Edna_Raul.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+
+        public DbSet<MaeModel> Mae { get; set; }
+        public DbSet<RecemNascidoModel> Bebe { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+            //Mapeamento da relação 1 para N entre genero e musica
+            modelBuilder.Entity<RecemNascidoModel>()
+                .HasOne(m => m.Genero)
+                .WithMany()
+                .HasForeignKey(m => m.GeneroId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+
+
+
+        }
+    }
+}
--- MaternidadeN1-Edna-Raul/MaternidadeN1-Edna-Raul/Models/RecemNascidoModel.cs
+++ MaternidadeN1-Edna-Raul/MaternidadeN1-Edna-Raul/Models/RecemNascidoModel.cs
@@ -2,5 +2,15 @@
 {
     public class RecemNascidoModel
     {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Genero { get; set; } = string.Empty;
+        public DateTime DataNascimento { get; set; }
+        public int Peso { get; set; }
+        public int Altura { get; set; }
+        public string TipoDeParto { get; set; } = string.Empty;
+        public int Apgar { get; set; }
+        public string CondicaoMedica { get; set; } = string.Empty;
+        public int MaeId { get; set; }
     }
 }
