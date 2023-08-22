096e9a3ac29e70437d79f4c09f8485bfda3c76ba
Edna de Paula <ednadepaulaeap@gmail.com>
2023-08-22 16:17:17 -0300

    services recemnascido criado

--- MaternidadeN1-Edna-Raul/MaternidadeN1-Edna-Raul/Interfaces/IRecemNascidoService.cs
+++ MaternidadeN1-Edna-Raul/MaternidadeN1-Edna-Raul/Interfaces/IRecemNascidoService.cs
@@ -1,6 +1,19 @@
-﻿namespace MaternidadeN1_Edna_Raul.Interfaces
+﻿using MaternidadeN1_Edna_Raul.DTOs;
+using MaternidadeN1_Edna_Raul.Models;
+
+namespace MaternidadeN1_Edna_Raul.Interfaces
 {
     public interface IRecemNascidoService
     {
+        Task<List<RecemNascidoModel>> GetRecemNascidoPorTipoParto(string tipoDeParto); //tipo de parto selecionando a mãe
+        Task<RecemNascidoModel> PostRecemNascido(RecemNascidoDTO recemNascidoRequest);
+        Task<RecemNascidoModel> UpdateRecemNascido(int id, RecemNascidoDTO recemNascidoRequest);
+        Task<RecemNascidoModel> DeleteRecemNascido(int id);
+        Task<List<RecemNascidoModel>> GetRecemNascidoPorGenero(string Genero);//selecionar bebe por genero selecionando a mãe
+        Task<RecemNascidoModel> GetRecemNascidoByID(int id);
+        Task<List<RecemNascidoModel>> GetRecemNascidoPorPeso(int peso);//selecionar bebe por peso selecionando a mãe
+
+        Task<List<RecemNascidoModel>> GetRecemNascidoPorApgar(int apgar);
+
     }
 }
---
+++ MaternidadeN1-Edna-Raul/MaternidadeN1-Edna-Raul/Services/RecemNascidoServices.cs
@@ -0,0 +1,54 @@
+﻿using MaternidadeN1_Edna_Raul.Data;
+using MaternidadeN1_Edna_Raul.DTOs;
+using MaternidadeN1_Edna_Raul.Interfaces;
+using MaternidadeN1_Edna_Raul.Models;
+
+namespace MaternidadeN1_Edna_Raul.Services
+{
+    public class RecemNascidoServices : IRecemNascidoService
+    {
+        private readonly DataContext _context;
+
+        public RecemNascidoServices(DataContext context) { _context = context; }
+
+        public Task<RecemNascidoModel> DeleteRecemNascido(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RecemNascidoModel> GetRecemNascidoByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<RecemNascidoModel>> GetRecemNascidoPorApgar(int apgar)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<RecemNascidoModel>> GetRecemNascidoPorGenero(string Genero)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<RecemNascidoModel>> GetRecemNascidoPorPeso(int peso)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<RecemNascidoModel>> GetRecemNascidoPorTipoParto(string tipoDeParto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RecemNascidoModel> PostRecemNascido(RecemNascidoDTO recemNascidoRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RecemNascidoModel> UpdateRecemNascido(int id, RecemNascidoDTO recemNascidoRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
