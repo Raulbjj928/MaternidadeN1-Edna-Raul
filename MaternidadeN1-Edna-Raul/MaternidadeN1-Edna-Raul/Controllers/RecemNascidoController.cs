008898dc496d524ab96edc0dafcae960f536e7f8
Edna de Paula <ednadepaulaeap@gmail.com>
2023-08-22 15:28:36 -0300

    data context atualuzado

---
+++ MaternidadeN1-Edna-Raul/MaternidadeN1-Edna-Raul/Controllers/RecemNascidoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MaternidadeN1_Edna_Raul.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecemNascidoController : ControllerBase
+    {
+    }
+}
